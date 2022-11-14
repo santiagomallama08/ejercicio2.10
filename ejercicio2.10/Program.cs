fa8a66737df804afd168b9fa1c3c2cce9617b484
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 19:01:42 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio2.10/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ejer4
+{
+    public class program
+    {
+        public static void Main()
+        {
+            double pulg = 0;
+            double mt= 0.0254;
+            double cpulg= 0;
+            double mdados= 0;
+            Console.WriteLine("digite la cantidad de metros necesitados");
+            mdados = double.Parse(Console.ReadLine());
+                cpulg = mdados / mt;
+            Console.WriteLine($"la cantidad de metros que debe pedir es de {cpulg}")
+        }
+    }
+}
